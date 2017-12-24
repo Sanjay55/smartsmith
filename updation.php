ecf4d92a1f74d3ade729a6fe8b895a7f49553126
Sanjay55 <sanjayvarma7506@gmail.com>
2017-12-24 15:37:54 +0530

    Updation Changes

--- updation.php
+++ updation.php
@@ -1,10 +1,17 @@
 <?php
 //require 'connect.php';
 
-$m = array("Cess ID"=>"3","Description"=>"product") ;
-$n = array("Cess ID"=>"3","Description"=>"awesome");
-$k = "cess class";
-updateRow($n,$m,$k,"Cess ID",3);
+//$m = array("Account ID"=>"1","Holder Name"=>"sanjay") ;
+//$n = array("Account ID"=>"1","Holder Name"=>"amit","ID"=>"4");
+//$k = "company bank accounts";
+/*
+@param  array $csvData Associative array of data in CSV File which is to be updated
+@param  array $tableData Associative array of data in table  which is going to be updated
+@param  string $tableName Name of the table in which the updation will be done 
+@param  string $commonKey common field between them whose values are same 
+@param  string $commonValue the equal value between them
+*/
+//updateRow($n,$m,$k,"Account ID",1);
 function updateRow($csvData,$tableData,$tableName,$commonKey,$commonValue){
 
 
@@ -12,8 +19,9 @@ function updateRow($csvData,$tableData,$tableName,$commonKey,$commonValue){
 	$username="root";
 	$password="";
 	$dbname="smartsoft";
-
+    //Create connection
 	$conn=mysqli_connect($server, $username, $password, $dbname);
+	//Check Connection
 	if(!$conn)
      die ("Connection failed:" .mysqli_connect_error());
     else
@@ -26,7 +34,7 @@ function updateRow($csvData,$tableData,$tableName,$commonKey,$commonValue){
     		if($tableKey != $commonKey && $tableKey == $key){
     		 $sql = "UPDATE `$tableName` SET `$tableKey`= '$updatedValue' WHERE `$commonKey` = '$commonValue'";
     		 
-    		 echo "<br>" .$sql;
+    		 //echo "<br>" .$sql;
     		 $run = mysqli_query($conn,$sql);
     	if(!$run)
     		echo "Updation Error <br>";
